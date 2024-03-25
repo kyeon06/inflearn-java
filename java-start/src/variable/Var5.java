ccc246c1dae5900b6aef83d3b6b2312b19b20c77
kyeon06 <jkyeon06@gmail.com>
2024-03-25 21:14:55 +0900

    :sparkles: 변수 선언과 초기화

---
+++ java-start/src/variable/Var5.java
@@ -0,0 +1,19 @@
+package variable;
+
+public class Var5 {
+    public static void main(String[] args) {
+        // 변수 선언, 초기화 각각 따로
+        int a;
+        a = 10;
+        System.out.println(a);
+
+        // 변수 선언과 초기화를 한번에
+        int b = 10;
+        System.out.println(b);
+
+        // 여러 변수 한꺼번에 초기화
+        int c = 1, d= 6;
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
---
+++ java-start/src/variable/Var6.java
@@ -0,0 +1,21 @@
+package variable;
+
+public class Var6 {
+    public static void main(String[] args) {
+        int a;
+        System.out.println(a);
+
+        /**
+         * 변수는 초기화 해야한다.
+         * java: variable a might not have been initialized
+         * 컴퓨터에서 메모리는 여러 시스템이 함께 사용하는 공간
+         * 변수를 선언하면 메모리상의 어떤 공간을 차지하고 사용하는데 그 공간에 기존에 어떤 값이 있는지는 모름
+         * 그래서 초기화를 하지 않으면 이상한 값이 출력될 수 있음
+         */
+
+        /**
+         * 컴파일 에러 : 자바 문법에 맞지 않을 때 발생하는 에러
+         * -- 명확하게 찾을 수 있기 때문에 사실 좋은 에러
+         */
+    }
+}
